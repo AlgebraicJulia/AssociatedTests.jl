a9783ec52645a45f912bbf72327d1b0dde7c93d5
Owen Lynch <root@owenlynch.org>
2024-08-07 17:37:58 -0700

    define tests by overloading module-local function

--- src/AssociatedTests.jl
+++ src/AssociatedTests.jl
@@ -2,47 +2,55 @@ module AssociatedTests
 
 using Test
 
-function test end
-export test
-
-const TESTS_FOR = Dict{Module, Set{Any}}()
-
-const TESTS_VAR = :__TESTS_VAR__
-
 @nospecialize
 function test(m::Module)
-  @testset verbose = true "$(nameof(m))" begin
-    for thing in names(m; all=true)
-      thing = getproperty(m, thing)
-      if thing != m
-        if hasmethod(test, (Val{thing},))
-          test(Val(thing))
-        elseif thing isa Module
-          test(thing)
+    @testset verbose = true "$(nameof(m))" begin
+        for method in methods(m.__runtest__)
+            m.__runtest__(method.sig.parameters[2]())
         end
-      end
+        for name in names(m; all=true)
+            thing = getproperty(m, name)
+            if thing isa Module && thing != m
+                test(thing)
+            end
+        end
     end
-  end
-  nothing
+    nothing
+end
+export test
+
+@nospecialize
+function test(m::Module, n::Module)
+    test(n)
+end
+
+@nospecialize
+function test(m::Module, f::Function)
+    m.__runtest__(Val(f))
 end
 
 @nospecialize
 function test(f::Function)
-  test(Val(f))
+    test(parentmodule(f), f)
+end
+
+@nospecialize
+function test(m::Module, d::Type)
+    m.__runtest__(Val(d))
 end
 
 @nospecialize
 function test(d::Type)
-  test(Val(d))
+    test(parentmodule(d), d)
 end
 
 macro tests(thing, block)
-  esc(quote
-    $(AssociatedTests).test(::Val{$thing}) = begin
-      $(Test).@testset $(string(thing)) $block
-      nothing
-    end
-  end)
+    esc(quote
+        __runtest__(::Val{$thing}) = begin
+            $(Test).@testset $(string(thing)) $block
+            nothing
+        end
+    end)
 end
 export @tests
 
